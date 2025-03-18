2f5c50e658cc9666e5790fef51866380ba0afa80
kid-us <kidushh29@gmail.com>
2025-03-18 11:39:17 +0300

    Pages directory created.

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <p>Welcome to About me</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/projects/pages.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectsPage = () => {
+  return (
+    <div>
+      <p>Welcome to my projects page</p>
+    </div>
+  );
+};
+
+export default ProjectsPage;
