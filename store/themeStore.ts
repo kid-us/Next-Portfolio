818b7133e1e7d3a717f83821a0900390b890dc6a
kid-us <kidushh29@gmail.com>
2025-03-19 16:18:40 +0300

    localStorage issue fixed.

--- store/themeStore.ts
+++ store/themeStore.ts
@@ -1,16 +1,38 @@
+// import { create } from "zustand";
+
+// interface State {
+//   theme: string;
+//   setTheme: (theme: string) => void;
+// }
+
+// const useThemeStore = create<State>((set) => ({
+//   theme: localStorage.getItem("theme") || "light",
+//   setTheme: (theme: string) => {
+//     localStorage.setItem("theme", theme);
+//     set({ theme });
+//   },
+// }));
+
+// export default useThemeStore;
+
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface State {
   theme: string;
   setTheme: (theme: string) => void;
 }
 
-const useThemeStore = create<State>((set) => ({
-  theme: localStorage.getItem("theme") || "light",
-  setTheme: (theme: string) => {
-    localStorage.setItem("theme", theme);
-    set({ theme });
-  },
-}));
+const useThemeStore = create<State>()(
+  persist(
+    (set) => ({
+      theme: "light", // Default theme
+      setTheme: (theme) => set({ theme }),
+    }),
+    {
+      name: "theme-storage", // Key for localStorage
+    }
+  )
+);
 
 export default useThemeStore;
